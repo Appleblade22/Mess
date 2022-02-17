b803331ca527382a9136c83e6e994c823d3c7ea8
Anand Thakur <75462622+Appleblade22@users.noreply.github.com>
2022-02-17 09:28:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 currentdate = new Date();
 let oneJan = new Date(currentdate.getFullYear(), 0, 1);
 let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000) - 1);
-let result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+let result = Math.ceil((numberOfDays) / 7);
 let eve_odd = result % 2;
 let day = currentdate.getDay();
 
